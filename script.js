b18318a11d6acd833e09ea9f2cf8777abc393ba3
Sarvarbek <hamkorbank.sms@gmail.com>
2022-05-15 19:00:00 +0500

    commit

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// // rest operator massivdagi malumotni itemga chiqarib beradi
+// function mySkills(lang, cms, ...rest) {
+//   console.log(lang, cms, rest);
+//   rest.forEach((item, i) => {
+//     console.log([0]);
+//     console.log(`extraskils  ${item}`);
+//   });
+// }
+// // rest operatoridagi rest element console.logda massiv ichida chiqadi
+// mySkills("Javascript", "word", "python, php, c++");
+
+// // default operatori
+
+// function calc(a, b = 2) {
+//   console.log(a + b);
+// }
+// calc(1);
+const root = document.querySelector(".root");
+
+function div(...classes) {
+  const elem = document.querySelector("div");
+  classes.forEach((classItem) => {
+    elem.classList.add(classItem);
+    root.append(elem);
+  });
+}
+div("width", "height", "bg");
